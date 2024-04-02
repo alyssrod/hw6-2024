711f58e626703929c37da77748bfc20fe839482e
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-02 18:28:02 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -6,80 +6,73 @@ window.addEventListener("load", function() {
 	video.autoplay = false;
 	video.loop = false;
 	console.log("Auto play is set to " + video.autoplay);
-	console.log ("Loop is set to " + video.loop)
+	console.log ("Loop is set to " + video.loop);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	video = document.querySelector("#player1");
-	volume_percent = video.volume * 100
+	var volume_percent = video.volume * 100;
 	video.play();
-	document.querySelector("#volume").innerHTML = volume_percent + "%"
+	document.querySelector("#volume").innerHTML = volume_percent + "%";
 	console.log("Played video");
-}
-);
+}); 
 
 document.getElementById("pause").addEventListener("click", function() {
     console.log("Pause Video");
     video.pause();
+}); 
+
+document.getElementById("slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log("New speed is " + video.playbackRate);
+}); 
+
+document.getElementById("faster").addEventListener("click", function() {
+    video.playbackRate /= 0.9;
+    console.log("New speed is " + video.playbackRate);
 });
 
-    document.getElementById("slower").addEventListener("click", function() {
-        video.playbackRate *= 0.9;
-        console.log("New speed is " + video.playbackRate);
-    });
-
-    document.getElementById("faster").addEventListener("click", function() {
-        video.playbackRate /= 0.9;
-        console.log("New speed is " + video.playbackRate);
-    });
-
-    document.querySelector("#skip").addEventListener("click", function() {
+document.querySelector("#skip").addEventListener("click", function() {
 	video = document.querySelector("#player1");
 
-	if (video.ended) {
+	if (video.currentTime + 10 >= video.duration) {
 		video.currentTime = 0;
-		console.log("Skipped ahead");
-		console.log("Video's current time is " + video.currentTime)
-	}
-	else {
+		console.log("Going back to the beginning");
+	} else {
 		video.currentTime += 10;
-		console.log("Skipped ahead");
-		console.log("Video's current time is " + video.currentTime)
 	}
-}
-);
+	console.log("Skipped ahead");
+	console.log("Video's current time is " + video.currentTime);
+}); // Corrected the logic for skipping and added missing closing parenthesis
 
 document.querySelector("#mute").addEventListener("click", function() {
 	video = document.querySelector("#player1");
 	
 	if (video.muted) {
 		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
 		console.log("Video is unmuted");
-		document.querySelector("#mute").innerHTML = "Mute"
 	}
 	else {
 		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
 		console.log("Video is muted");
-		document.querySelector("#mute").innerHTML = "Unmute"
 	}
-}
-);
+}); 
 
-    document.querySelector("#slider").addEventListener("input", function() {
-        video = document.querySelector("#player1");
-	volume = document.querySelector("#slider").value;
-	volume_decimal = volume / 100
+document.querySelector("#slider").addEventListener("input", function() {
+    video = document.querySelector("#player1");
+	var volume = document.querySelector("#slider").value;
+	var volume_decimal = volume / 100;
 	video.volume = volume_decimal;
-	document.querySelector("#volume").innerHTML = volume + "%"
-	console.log("The volume value is " + volume_decimal)
-    }
-    );
-    document.querySelector("#vintage").addEventListener("click", function() {
-        video.classList.add("oldSchool");
-    }
-    );
+	document.querySelector("#volume").innerHTML = volume + "%";
+	console.log("The volume value is " + volume_decimal);
+}); 
 
-    document.querySelector("#orig").addEventListener("click", function() {
-        video.classList.remove("oldSchool");
-    }
-    );
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.add("oldSchool");
+}); 
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+}); 
